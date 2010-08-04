01e46f8ae5fcae87d1ee53941d8cc8f3b679eea1
Ed Schmalzle <Jonas714@gmail.com>
2010-08-04 11:01:00 -0400

    use --long flag so we get whole key values

--- lib/habitat/railtie.rb
+++ lib/habitat/railtie.rb
@@ -4,7 +4,7 @@ require 'rails'
 module Habitat
   class HabitatRailtie < Rails::Railtie
     initializer "Habitat.configure_heroku_environment_variables" do
-      heroku_config = `heroku config`
+      heroku_config = `heroku config --long`
       heroku_config = heroku_config.split("\n").map{|string| string.split("=>").map(&:strip)}
       heroku_config.each do |conf|
         ENV[conf.first] = conf.last
