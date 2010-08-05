e26294d44e23760d6d19e12a14b42dde732354d1
Ed Schmalzle <Jonas714@gmail.com>
2010-08-05 08:25:06 -0400

    Allow user to override heroku config with local yaml file.

--- lib/habitat/railtie.rb
+++ lib/habitat/railtie.rb
@@ -6,8 +6,15 @@ module Habitat
     initializer "Habitat.configure_heroku_environment_variables" do
       heroku_config = `heroku config --long`
       heroku_config = heroku_config.split("\n").map{|string| string.split("=>").map(&:strip)}
-      heroku_config.each do |conf|
-        ENV[conf.first] = conf.last unless conf.first == "RACK_ENV"
+      heroku_config = heroku_config.inject({}){|memo, entry| memo[entry.first] = entry.last;memo;}
+
+      override_path = Rails.root.join('config','habitat.yml')
+      override_config = YAML.load_file(override_path) if File.exists?(override_path)
+
+      heroku_config.merge!(override_config)
+
+      heroku_config.each_pair do |key, value|
+        ENV[key] = value unless key == "RACK_ENV"
       end
     end
   end
